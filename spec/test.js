a8afb28b5a97eb135affdcf0f7ca2c638c70e478
Julian V <jvenci@comcast.net>
2019-04-02 16:12:29 -0700

    Correct minor syntax errors and add two 'build' options in package.json, one with --w flag and one without

--- spec/test.js
+++ spec/test.js
@@ -6,7 +6,6 @@ import App from '../client/src/App';
 describe('App', () => {
   it('should contain text', () => {
     const wrapper = shallow(<App />);
-    // const text = wrapper.find('div div');
     expect(wrapper.find("#test")).toHaveText("Hello World, I'm a ReactJS component, aren't you jealous?");
   })
 })
